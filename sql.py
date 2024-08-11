1463654dc6c0c2c318d1cbc018ba0c4016689eca
alex <avaymetov@gmail.com>
2024-08-11 19:31:12 +0500

    feat: concurents_today, top10_offer_today, all_offer

--- sql.py
+++ sql.py
@@ -1,6 +1,7 @@
 from config import host, user, password, db_name
 import sqlalchemy
 import pandas as pd
+from datetime import datetime
 
 def top10_offer():
     engine = sqlalchemy.create_engine(f'mysql+pymysql://{user}:{password}@{host}/{db_name}')
@@ -24,4 +25,34 @@ def concurents():
     insiders['views_all'] = list_insiders2
     insiders['count_offers'] = list_insiders3
     df = pd.DataFrame(insiders)
+    return df
+
+def concurents_today():
+    engine = sqlalchemy.create_engine(f'mysql+pymysql://{user}:{password}@{host}/{db_name}')
+    sql = "SELECT * FROM `user_data`"
+    df = pd.read_sql_query(sql, engine)
+    insiders = {}
+    list_insiders1 = []
+    list_insiders2 = []
+    list_insiders3 = []
+    for i in df.category.unique():
+        list_insiders1.append(i)
+        list_insiders2.append(int(df.loc[df['category'] == i, 'views_today'].sum()))
+        list_insiders3.append(len(df.loc[(df['category'] == i) & (df['publish_date'] == datetime.now().date())]))
+    insiders['category'] = list_insiders1
+    insiders['views_today'] = list_insiders2
+    insiders['count_offers'] = list_insiders3
+    df = pd.DataFrame(insiders)
+    return df
+
+def top10_offer_today():
+    engine = sqlalchemy.create_engine(f'mysql+pymysql://{user}:{password}@{host}/{db_name}')
+    sql = "SELECT * FROM `user_data`"
+    df = pd.read_sql_query(sql, engine)
+    return df.nlargest(10, ['views_today'])
+
+def all_offer():
+    engine = sqlalchemy.create_engine(f'mysql+pymysql://{user}:{password}@{host}/{db_name}')
+    sql = "SELECT * FROM `user_data`"
+    df = pd.read_sql_query(sql, engine)
     return df
